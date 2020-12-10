636023b78dda3d7e83ac2eb6aa65ae029b7c082a
Nivek <54602828+kyeung7@users.noreply.github.com>
2020-12-10 16:00:28 -0500

    Started Q21

---
+++ Q21 - Merge Two Sorted Lists.py
@@ -0,0 +1,17 @@
+# Merge two sorted linked lists and return it as a new sorted list.
+# The new list should be made by splicing together the nodes of the first two lists.
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# Runtime:- ms, Memory: - MB
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
