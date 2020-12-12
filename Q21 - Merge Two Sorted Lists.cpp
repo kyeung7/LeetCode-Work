e9b2e04ce7f5d22941267d714a8db894d62889f5
Nivek <54602828+kyeung7@users.noreply.github.com>
2020-12-12 11:36:51 -0500

    Changed q21 to cpp

---
+++ Q21 - Merge Two Sorted Lists.cpp
@@ -0,0 +1,20 @@
+// Merge two sorted linked lists and return it as a new sorted list.
+// The new list should be made by splicing together the nodes of the first two lists.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+# Runtime:- ms, Memory: - MB
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        
+    }
+};
