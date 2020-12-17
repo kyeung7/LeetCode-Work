4f65f58036042e67369178ede0f2ae29e5360271
Nivek <54602828+kyeung7@users.noreply.github.com>
2020-12-17 11:50:49 -0500

    Completed Q28

--- Q28 - Implement strStr().py
+++ Q28 - Implement strStr().py
@@ -1,26 +1,28 @@
+# Return the index of the first occurrence of needle in haystack
+# Return -1 if needle is not part of haystack.
+# Return 0 if exceeds size bounds
 
-
-# Results: Runtime: 16ms, Memory: 13.4MB
+# Results: Runtime: 388ms, Memory: 14.6MB
 class Solution(object):
     def strStr(self, haystack, needle):
+
+        # Range constaintsm 
+        if len(needle) <= 0 or len(needle) >= 5*(10**4):
+            return 0
+        
+        # Iterate through haystack
+        for i in range(len(haystack)):
+            
+            # Using needle size, look at sets of characters in haystack, return index if found
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        
+        # Otherwise not found
+        return -1
+    
+    
         """
         :type haystack: str
         :type needle: str
         :rtype: int
         """
-        print("needle: ", needle, " size: ", len(needle))
-        if len(needle) == 0:
-            return 0
-        
-        i = 0
-        while i < len(haystack):
-            
-            if haystack[i:i+len(needle)] == needle:
-                print("match found at index: ", i)
-                return i
-                
-            print(haystack[i: i+len(needle)])
-            
-            i+=1
-        
-        return -1
