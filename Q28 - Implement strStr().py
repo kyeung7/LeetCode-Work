6df9b8b06a36db742f3af0f7114c08fda482c69d
Nivek <54602828+kyeung7@users.noreply.github.com>
2020-12-26 15:47:50 -0500

    yep

--- Q28 - Implement strStr().py
+++ Q28 - Implement strStr().py
@@ -2,3 +2,21 @@
 
 # Results: Runtime: 16ms, Memory: 13.4MB
 class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        sizeCheck = len(needle)
+        print(sizeCheck)
+        
+        #string of needle
+        #empty string thats updated if matches found
+        tempStr = ""
+        
+        for char in haystack:
+            if char in needle:
+                tempStr += char
+                if tempStr==needle:
+                    pass # now return index of instance found
