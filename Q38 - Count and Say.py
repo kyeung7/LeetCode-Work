567dd2738f7bc8257de666c6c40f7962fc36740c
Nivek <54602828+kyeung7@users.noreply.github.com>
2020-12-27 10:49:57 -0500

    Update Q38 - Count and Say.py

---
+++ Q38 - Count and Say.py
@@ -0,0 +1,5 @@
+# Given a sorted array of distinct integers and a target value
+# Return the index if the target is found.
+# If not, return the index where it would be if it were inserted in order.
+
+ 
