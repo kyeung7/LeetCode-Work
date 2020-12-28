e1ea24ceb9c6ab9235f436147eaa1764ee86b42e
Nivek <54602828+kyeung7@users.noreply.github.com>
2020-12-28 11:22:25 -0500

    Update Q38 - Count and Say.py

--- Q38 - Count and Say.py
+++ Q38 - Count and Say.py
@@ -2,4 +2,11 @@
 # Return the index if the target is found.
 # If not, return the index where it would be if it were inserted in order.
 
- 
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
