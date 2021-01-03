c23d4f6497337f391d87ab8c7f4ffbb320376af6
Nivek <54602828+kyeung7@users.noreply.github.com>
2021-01-03 13:47:16 -0500

    Create Q131 - Fibonacci Number.kt

---
+++ Q131 - Fibonacci Number.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun fib(n: Int): Int {
+        return val 0 = if (n > 30) {
+            "n more than 30"
+        }
+        
+        return val 0 = if (n < 0) {
+            "n less than 0"
+        }
+        
+        return val 0 = if (n = 1) {
+            "n = 1"
+        }
+
+        return fib(n - 1) + fib(n - 2)
+    }
+}
